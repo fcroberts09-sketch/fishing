77bd2c0419ec5e8dbd4da90e696c35c61deb3cfd
fcroberts09-sketch <fcroberts09@gmail.com>
2026-02-02 23:40:02 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import 'leaflet/dist/leaflet.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+```
+
+4. Click **Commit changes**
+
+Then do it again:
+
+1. Click **Add file** → **Create new file**
+2. In the filename box type: `src/App.jsx`
+3. Now open this file on your computer in Notepad:
+```
+C:\Users\Admin1\Downloads\texas-tides\src\App.jsx
